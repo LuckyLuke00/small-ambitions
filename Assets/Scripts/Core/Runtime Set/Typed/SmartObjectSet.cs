386e6b75bf047c51f51e9263b8264dd4a3297ca7
LuckyLuke00 <lucas.kinoo5@gmail.com>
2025-12-20 16:49:05 +0100

    Revert IK Changes

--- Assets/Scripts/Core/Runtime Set/Typed/SmartObjectSet.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace SmallAmbitions
-{
-    [CreateAssetMenu(menuName = "Small Ambitions/Runtime Sets/Smart Object Set", fileName = "SmartObjectSet")]
-    public class SmartObjectSet : RuntimeSet<SmartObject>
-    {
-    }
-}
