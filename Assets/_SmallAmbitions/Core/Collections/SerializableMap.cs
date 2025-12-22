427335b30610733bea9730ac83c8c593af064475
LuckyLuke00 <lucas.kinoo5@gmail.com>
2025-12-22 15:07:54 +0100

    Implement a SerializableMap and related editor script

---
+++ Assets/_SmallAmbitions/Core/Collections/SerializableMap.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SmallAmbitions
+{
+    [Serializable]
+    public sealed class SerializableMap<TKey, TValue> : ISerializationCallbackReceiver, IEnumerable<KeyValuePair<TKey, TValue>> where TKey : unmanaged
+    {
+        [Serializable]
+        private struct Entry
+        {
+            public TKey Key;
+            public TValue Value;
+        }
+
+        [SerializeField] private List<Entry> _entries = new();
+        private Dictionary<TKey, TValue> _dictionary = new();
+
+        #region ISerializationCallbackReceiver
+
+        public void OnBeforeSerialize()
+        { /* Intentionally empty: duplicates in _entries are resolved in OnAfterDeserialize via TryAdd */ }
+
+        public void OnAfterDeserialize()
+        {
+            _dictionary.Clear();
+            _dictionary.EnsureCapacity(_entries.Count);
+
+            foreach (var entry in _entries)
+            {
+                _dictionary.TryAdd(entry.Key, entry.Value);
+            }
+        }
+
+        #endregion ISerializationCallbackReceiver
+
+        #region IEnumerable<KeyValuePair<TKey, TValue>>
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _dictionary.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        #endregion IEnumerable<KeyValuePair<TKey, TValue>>
+    }
+}
---
+++ Assets/_SmallAmbitions/Editor/SerializableMapDrawer.cs
@@ -0,0 +1,83 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace SmallAmbitions.Editor
+{
+    [CustomPropertyDrawer(typeof(SerializableMap<,>), useForChildren: true)]
+    public sealed class SerializableMapDrawer : PropertyDrawer
+    {
+        private const string EntriesFieldName = "_entries";
+        private const string KeyFieldName = "Key";
+
+        private const string WarningIconName = "console.warnicon.sml";
+        private const string WarningMessage = "Duplicate keys detected. Only the first occurrence is used.";
+
+        private static readonly GUIStyle WarningStyle = EditorStyles.helpBox;
+        private static readonly GUIContent WarningContent = new(WarningMessage, EditorGUIUtility.IconContent(WarningIconName).image);
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var propertyHeight = EditorGUI.GetPropertyHeight(property, label, true);
+            var propertyRect = new Rect(position.x, position.y, position.width, propertyHeight);
+
+            EditorGUI.PropertyField(propertyRect, property, label, true);
+
+            if (!HasDuplicateKeys(property))
+            {
+                return;
+            }
+
+            propertyRect.y = propertyRect.yMax + EditorGUIUtility.standardVerticalSpacing;
+            propertyRect.height = GetWarningHeight(position.width);
+
+            EditorGUI.LabelField(propertyRect, WarningContent, WarningStyle);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var height = EditorGUI.GetPropertyHeight(property, label, includeChildren: true);
+
+            if (HasDuplicateKeys(property))
+            {
+                height += EditorGUIUtility.standardVerticalSpacing;
+                height += GetWarningHeight(EditorGUIUtility.currentViewWidth);
+            }
+
+            return height;
+        }
+
+        private static float GetWarningHeight(float width)
+        {
+            return WarningStyle.CalcHeight(WarningContent, width);
+        }
+
+        private static bool HasDuplicateKeys(SerializedProperty mapProperty)
+        {
+            var entries = mapProperty.FindPropertyRelative(EntriesFieldName);
+            if (entries == null || !entries.isArray)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < entries.arraySize; ++i)
+            {
+                var keyA = entries.GetArrayElementAtIndex(i)?.FindPropertyRelative(KeyFieldName);
+                if (keyA == null)
+                {
+                    continue;
+                }
+
+                for (int j = i + 1; j < entries.arraySize; ++j)
+                {
+                    var keyB = entries.GetArrayElementAtIndex(j)?.FindPropertyRelative(KeyFieldName);
+                    if (keyB != null && SerializedProperty.DataEquals(keyA, keyB))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
