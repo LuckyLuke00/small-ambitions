fd8060c7b12949b6b8f2564d48d0b10df7798a59
LuckyLuke00 <lucas.kinoo5@gmail.com>
2025-12-24 16:09:47 +0100

    Create a SmartObjectRuntimeSet and integrate with SmartObject

---
+++ Assets/_SmallAmbitions/Gameplay/SmartObjects/Data/SmartObjectRuntimeSet.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace SmallAmbitions
+{
+    [CreateAssetMenu(menuName = "Small Ambitions/Runtime Sets/Smart Object Runtime Set", fileName = "SmartObjectRuntimeSet")]
+    public sealed class SmartObjectRuntimeSet : RuntimeSet<SmartObject>
+    {
+    }
+}
--- Assets/_SmallAmbitions/Gameplay/SmartObjects/Scripts/SmartObject.cs
+++ Assets/_SmallAmbitions/Gameplay/SmartObjects/Scripts/SmartObject.cs
@@ -6,7 +6,18 @@ namespace SmallAmbitions
     public sealed class SmartObject : MonoBehaviour
     {
         [SerializeField] private SerializableMap<InteractionSlotType, Transform> _interactionSlots = new();
+        [SerializeField] private SmartObjectRuntimeSet _smartObjects;
 
         public IReadOnlyDictionary<InteractionSlotType, Transform> InteractionSlots => _interactionSlots;
+
+        private void OnEnable()
+        {
+            _smartObjects.Add(this);
+        }
+
+        private void OnDisable()
+        {
+            _smartObjects.Remove(this);
+        }
     }
 }
