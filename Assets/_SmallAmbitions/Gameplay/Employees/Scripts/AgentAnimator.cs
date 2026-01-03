e6a3e871fede51a85633f90c8ef1dbb5663471af
LuckyLuke00 <lucas.kinoo5@gmail.com>
2026-01-03 14:57:38 +0100

    Add simple one-shot animation support

--- Assets/_SmallAmbitions/Gameplay/Employees/Scripts/AgentAnimator.cs
+++ Assets/_SmallAmbitions/Gameplay/Employees/Scripts/AgentAnimator.cs
@@ -4,19 +4,44 @@ using UnityEngine.AI;
 namespace SmallAmbitions
 {
     [RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
-    public class AgentAnimator : MonoBehaviour
+    public sealed class AgentAnimator : MonoBehaviour
     {
         [Header("References")]
-        [SerializeField] private NavMeshAgent _navMeshAgent;
         [SerializeField] private Animator _animator;
+        [SerializeField] private NavMeshAgent _navMeshAgent;
 
-        [Header("Animator Parameters")]
+        [Header("One-Shot Animation")]
+        [SerializeField] private AnimationClip _oneShotPlaceholder;
+        [SerializeField] private AnimatorParameter _oneShotTrigger;
+
+        [Header("Locomotion")]
         [SerializeField] private AnimatorParameter _speed;
+
+        private AnimatorOverrideController _oneShotOverride;
 
         private void Update()
         {
             float speedPercent = MathUtils.SafeDivide(_navMeshAgent.velocity.magnitude, _navMeshAgent.speed);
             _animator.SetFloat(_speed.Hash, speedPercent);
+        }
+
+        public void PlayOneShot(AnimationClip clip)
+        {
+            if (clip == null)
+            {
+                Debug.LogWarning($"[{nameof(AgentAnimator)}] Cannot play one-shot animation: clip is null.");
+                return;
+            }
+
+            if (_oneShotOverride == null)
+            {
+                _oneShotOverride = new AnimatorOverrideController(_animator.runtimeAnimatorController);
+                _animator.runtimeAnimatorController = _oneShotOverride;
+            }
+
+            _oneShotOverride[_oneShotPlaceholder] = clip;
+            _animator.ResetTrigger(_oneShotTrigger.Hash);
+            _animator.SetTrigger(_oneShotTrigger.Hash);
         }
     }
 }
